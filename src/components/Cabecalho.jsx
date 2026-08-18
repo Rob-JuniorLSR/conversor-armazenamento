c9726c570295eb73f779328eae7c42a100e31d89
Robson Junior <regorobson631@gmail.com>
2026-08-18 19:21:07 -0300

    Update Cabecalho.jsx

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,1 +1,10 @@
+import React from 'react';
 
+export default function Cabecalho() {
+  return (
+    <header>
+      <h1>Conversor de Armazenamento</h1>
+      <p>Converta facilmente valores entre Bytes, KB, MB, GB e TB</p>
+    </header>
+  );
+}
