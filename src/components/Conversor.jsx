0c0bf104a16ea20e2aa5727ca36303d8f0068d15
Robson Junior <regorobson631@gmail.com>
2026-08-19 21:15:37 -0300

    Update Conversor.jsx

--- src/components/Conversor.jsx
+++ src/components/Conversor.jsx
@@ -1,1 +1,21 @@
+function Conversor() {
+  return (
+    <main>
+      <label>Valor:</label>
+      <input type="number" />
 
+      <label>De:</label>
+      <select>
+        <option>Bytes</option>
+        <option>KB</option>
+        <option>MB</option>
+        <option>GB</option>
+        <option>TB</option>
+      </select>
+
+      <button>Converter</button>
+    </main>
+  )
+}
+
+export default Conversor
